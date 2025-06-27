62a077de2695af858cb3356ccfe492f2f8688b1c
Rishikesh P C <rishikeshpc4545@gmail.com>
2025-06-27 16:40:54 +0530

    completed the making of game and finished game score logic

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let random,myinput;
 let round = 0;
 let player_select,computer_select
 let player_score=0,computer_score = 0;
+let round_result = '';
 
 
 // getting computer choice
@@ -34,46 +35,95 @@ function Round() {
         player_select == "paper" && computer_select == "paper" ||
         player_select == "scissor" && computer_select == "scissor"
     ) {
-        console.log("same");
         console.log(`player selection : ${player_select}`);
         console.log(`computer selection : ${computer_select}`);
+        round_result = 'same'
+        console.log("same");
     }
     else if (player_select == "rock" && computer_select == "paper"){
-        console.log("computer win");
         console.log(`player selection : ${player_select}`);
         console.log(`computer selection : ${computer_select}`);
+        round_result = "computer_win";
+        console.log("computer win");
     }
     else if (player_select == "rock" && computer_select == "scissor"){
-        console.log("player win");
         console.log(`player selection : ${player_select}`);
         console.log(`computer selection : ${computer_select}`);
+        round_result = "player_win";
+        console.log("player win");
     }
     else if (player_select == "paper" && computer_select == "rock"){
-        console.log("player win");
         console.log(`player selection : ${player_select}`);
         console.log(`computer selection : ${computer_select}`);
+        round_result = "player_win";
+        console.log("player win");
     }
     else if (player_select == "paper" && computer_select == "scissor"){
-        console.log("computer win");
         console.log(`player selection : ${player_select}`);
         console.log(`computer selection : ${computer_select}`);
+        round_result = "computer_win";
+        console.log("computer win");
     }
     else if (player_select == "scissor" && computer_select == "rock"){
-        console.log("computer win");
         console.log(`player selection : ${player_select}`);
         console.log(`computer selection : ${computer_select}`);
+        round_result = "computer_win";
+        console.log("computer win");
     }
     else if (player_select == "scissor" && computer_select == "paper"){
-        console.log("player win");
         console.log(`player selection : ${player_select}`);
         console.log(`computer selection : ${computer_select}`);
+        console.log("player win");
+        round_result = "player_win";
     }
-    console.log(`player score ${player_score} & computer score  ${computer_score}`);
-    console.log(round);
+    else {
+        console.log(`player selection : ${player_select}`);
+        console.log(`computer selection : ${computer_select}`);
+        console.log("wrong input");
+        round_result = "none";
+    }
+
+    // <-- logic done -->
+
+    console.log(`number of rounds ${round}`);
 }
 
-Round()
+// making 5 rounds
+function loopround() {
+    for (let i=1;i<=5;i++) {
+        Round()
+        // adding score
+        if (round_result == "none") {
+            player_score = player_score+0;
+            computer_score = computer_score +0;
+        }
+        else if (round_result == "player_win") {
+            player_score = player_score+1;
+            computer_score = computer_score +0;            
+        }
+        else if (round_result == "computer_win") {
+            player_score = player_score+0;
+            computer_score = computer_score +1;  
+        }
+        console.log("");
+        
+    }
 
+    // making the final result
+    console.log(`player score : ${player_score}`);
+    console.log(`computer score : ${computer_score}`);
+
+    if (player_score > computer_score) {
+        alert("player win's the game")
+    }
+    else if (player_score < computer_score) {
+        alert("computer wins the game")
+    }else {
+        alert("the game ended in a draw")
+    }
+}
+
+loopround()
 
 
 
